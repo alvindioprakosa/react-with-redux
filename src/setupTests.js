5a488051adb72f5f55a7903fc3de298607c59f05
alvindioprakosa <alvindioprakosa@gmail.com>
2025-03-28 22:04:19 +0700

    Update Setup Test

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,12 +1,4 @@
 // src/setupTests.js
 
-// Jest DOM menambahkan custom matchers untuk mempermudah pengujian elemen DOM.
-// Ini memungkinkan kita untuk melakukan asersi yang lebih spesifik pada konten dan atribut elemen.
-// Contoh penggunaan:
-// expect(element).toHaveTextContent(/react/i)  -> Memastikan elemen memiliki teks yang mengandung "react" (case insensitive)
-// expect(button).toBeDisabled()                -> Memastikan tombol dalam keadaan nonaktif (disabled)
-// expect(link).toHaveAttribute('href', '/home') -> Memastikan link memiliki atribut href yang tepat
-
-// Dokumentasi lebih lanjut dapat dilihat di: 
-// https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+// Mengimpor jest-dom untuk menambahkan custom matchers dalam pengujian React.
+import "@testing-library/jest-dom";
